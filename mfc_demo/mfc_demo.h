55326d25a80ca3b15c43241ee58173750389d920
WuChao <wuchao01@koolearn.com>
2026-04-25 07:39:48 +0800

    feat: 添加连连看游戏核心功能实现
    实现连连看游戏的核心逻辑，包括：
    1. 添加游戏板初始化、洗牌和消除功能
    2. 实现三种连接路径检测算法
    3. 添加游戏状态检查和胜负判断
    4. 实现游戏界面绘制和鼠标交互
    5. 添加游戏设置对话框和开始菜单

--- mfc_demo/mfc_demo.h
+++ mfc_demo/mfc_demo.h
@@ -1,3 +1,19 @@
 ﻿#pragma once
 
 #include "resource.h"
+#include "LinkGame.h"
+
+#define TILE_SIZE 50
+
+extern HWND g_hWnd;
+extern CLinkGame g_linkGame;
+extern int g_boardWidth;
+extern int g_boardHeight;
+extern bool g_gameStarted;
+extern Point g_selectedTile;
+
+INT_PTR CALLBACK SettingsDlg(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+void DrawBoard(HDC hdc);
+void DrawTile(HDC hdc, int x, int y, int iconIndex);
+void OnLButtonDown(HWND hWnd, int x, int y);
+void CheckGameState();
